2ff3f5522d504a57e00b0edd8ff2d48dce7f2243
Gabriel Passos <kooujinproductions@gmail.com>
2022-06-02 02:11:47 -0300

    Inicial commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,21 @@
+export function Header() {
+  return (
+    <header className="flex h-20 bg-[#333333] justify-between">
+      <img src="../../../public/images/logo.svg" alt="" className="h-24 w-44 relative pb-4 pl-4" />
+      <div className="flex items-center">
+        <nav className="">
+          <a href="" className="pl-28 pr-8 text-gray-400 hover:text-gray-200 transition-colors">Início</a>
+          <a href="" className="pr-8 text-gray-400 hover:text-gray-200 transition-colors">Quem somos</a>
+          <a href="" className="pr-8 text-gray-400 hover:text-gray-200 transition-colors">Onde estamos</a>
+          <a href="" className="pr-8 text-gray-400 hover:text-gray-200 transition-colors">Contato</a>
+        </nav>
+      </div>
+      <div className="pr-16 pt-[1.1rem]">
+        <button className="border-solid border border-zinc-400 rounded-[3rem] justify-items-center py-1.5 px-7 text-zinc-300 font-bold text-lg">
+        Fazer Orçamento
+        </button>
+      </div>
+      
+    </header>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.tsx"],
+  theme: {
+    borderRadius: {
+      'button': '35rem'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
